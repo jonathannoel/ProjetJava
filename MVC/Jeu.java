44184de763223425e3517bf03da14b4e12f23b13
Rey Tshiama <pm.tshiama@gmail.com>
2016-12-06 14:11:18 +0100

    Update Jeu.java

--- MVC/Jeu.java
+++ MVC/Jeu.java
@@ -1,106 +1,51 @@
-package MVC;
+package view;
 
-import java.util.Scanner;
-
-import model.Bateau;
-import controller.CarteController;
-import controller.InteractionController;
-import controller.JoueurController;
 import model.Carte;
-import model.InteractionsUtilisateur;
-import model.Joueur;
-import view.CarteView;
-import view.InteractionView;
-import view.JoueurView;
 
-public class Jeu {
-	public static void main(String[] args) throws InterruptedException {
-		Scanner reader = new Scanner(System.in);
-		Bateau[] bateaux;
-		int input = 0;
-		
-		/*----------USER-----------*/
-		//fetch student record based on his roll no from the database
-		Joueur joueurModel  = retriveJoueurFromDatabase();
-		//Create a view : to write student details on console
-		JoueurView joueurView = new JoueurView();
-		JoueurController joueurController = new JoueurController(joueurModel, joueurView);
-		
-		/*--------- MAP -------*/
-		Carte carteModel = new Carte();
-		CarteView carteView = new CarteView();
-		CarteController carteController = new CarteController(carteModel,carteView);
-		joueurController.updateView();
-		/** for(int i = 3; i > 0; i--){
-			System.out.println("le jeu va commencer dans : " + i);
-			Thread.sleep(1000);
-		} **/
-		carteModel.setUser(joueurModel.getName());
-		carteController.updateView();
-		
-		/*----------Interaction Utilisateur --------*/
-		InteractionsUtilisateur iuModel = new InteractionsUtilisateur();
-		InteractionView iuView = new InteractionView();
-		InteractionController iuController = new InteractionController(iuModel, iuView);
-
-		int i = 0;
-		do {
-			Bateau b;
-			iuController.updateView(3);
-
-			try {
-				while(input != 1 && input != 2){
-					System.out.println("Veuillez introduire 1 ou 2");
-					input = Integer.parseInt(reader.next());
-				}
-			} catch (NumberFormatException e){
-				System.out.println("Votre réponse n'est pas valable");
-			}
-			
-			if(input == 1){
-				iuController.updateView(0);
-				iuController.demandeCellule();
-				b = new Bateau(iuController.getIntCaseIntroduite()[0], iuController.getIntCaseIntroduite()[1], false, 3);
-				
-				carteController.drawBateau(b);
-				
-				if(carteController.isOk()){
-					i++;
-				}
-				carteController.setOk(false);
-				
-				input = 0;
-			}
-			
-			if(input == 2){
-				iuController.updateView(0);
-				iuController.demandeCellule();
-				b = new Bateau(iuController.getIntCaseIntroduite()[0], iuController.getIntCaseIntroduite()[1], true, 3);
-				
-				carteController.drawBateau(b);
-				
-				if(carteController.isOk()){
-					i++;
-				}
-				
-				carteController.setOk(false);
-				
-				input = 0;
-			}			
-		} while (i < 3);
-		
-		while(true){
-			iuController.updateView(1);
-			iuController.demandeCellule();
-			carteController.tir(iuController.getIntCaseIntroduite());
-			carteController.updateView();
-			carteController.randomTir();
-			carteController.updateView();
-			carteController.showAdversaireAttaque();
+public class CarteView extends Carte{
+	public void afficherCarte(int[][] carte1, int[][] carte2, String user ){
+		int count = 0;
+		char a = 'A';
+		System.out.println("Adversaire : \n");
+		while(count<carte1.length){
+			System.out.print("\t"+ a);
+			a++;
+			count++;
 		}
+		System.out.println();
+        for(int i=0 ; i < carte1.length ; i++ ){
+            System.out.print((i+1)+"");
+            for(int j=0 ; j < carte1.length ; j++ ){
+                switch(carte1[i][j]){
+	                case(0):System.out.print("\t"+"~");break;
+	                case(1): System.out.print("\t"+"*");break;
+	                case(2):System.out.print("\t"+"X");break;
+	                case(3):System.out.print("\t" + "~");break;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("\n"+user+"\n");
+        a = 'A';
+        count = 0;
+        while(count<carte1.length){
+			System.out.print("\t"+ a);
+			a++;
+			count++;
+		}
+        System.out.println();
+        for(int i=0 ; i < carte2.length ; i++ ){
+            System.out.print((i+1)+"");
+            for(int j=0 ; j < carte2.length ; j++ ){
+                switch(carte2[i][j]){
+	                case(0):System.out.print("\t"+"~");break;
+	                case(1): System.out.print("\t"+"*");break;
+	                case(2):System.out.print("\t"+"X");break;
+	                case(3):System.out.print("\t" + "B");break;
+                }
+                
+            }
+            System.out.println();
+        }
 	}
-   private static Joueur retriveJoueurFromDatabase(){
-      Joueur joueur1 = new Joueur();
-      return joueur1;
-   }
 }
