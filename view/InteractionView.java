6219d25a668f2f8944e90de6cf136b4907ffb09d
Rey Tshiama <pm.tshiama@gmail.com>
2016-12-06 14:23:39 +0100

    Update InteractionView.java

--- view/InteractionView.java
+++ view/InteractionView.java
@@ -6,13 +6,13 @@ public class InteractionView extends InteractionsUtilisateur {
 	public void afficherInstruction(int i){
 		switch(i){
 			case(0) : 
-				System.out.println("Où voulez-vous mettre votre bateau ?");
+				System.out.println("OÃ¹ voulez-vous mettre votre bateau ?");
 				break;
 			case(1) : 
 				System.out.println("Quelle cellule voulez-vous attaquer ?");
 				break;
 			case(2) : 
-				System.out.println("Veuillez introduire une lettre(A-F), puis un chiffre(1-10)");
+				System.out.println("Veuillez introduire une lettre(A-J), puis un chiffre(1-10)");
 				break;
 			case(3) : 
 				System.out.println("Tapez 1 pour un bateau horizontal, 2 pour un vertical");
